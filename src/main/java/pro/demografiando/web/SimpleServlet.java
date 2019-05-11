c38ff8fe8599f42a46137cfbbe2c7e0d81c834be
ksaweryn <javier.borja.moretta@gmail.com>
2019-05-11 18:35:42 -0500

    Servlet added

---
+++ src/main/java/pro/demografiando/web/SimpleServlet.java
@@ -0,0 +1,17 @@
+package pro.demografiando.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SimpleServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("SimpleServlet Executed");
+        out.flush();
+        out.close();
+    }
+
+}
